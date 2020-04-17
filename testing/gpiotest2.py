1fb1baa339ec916bf402d763e97aecbdbd2acaf7
3lorsin <51426836+3lorsin@users.noreply.github.com>
2020-04-16 19:54:48 -0500

    added testing folder

--- testing/gpiotest2.py
+++ testing/gpiotest2.py
@@ -6,14 +6,14 @@ def onButton(channel):
     if channel == 23:
         print("Button",channel,"was pressed!")
 
-# Setup GPIO16 as input with internal pull-up resistor to hold it HIGH
+# Setup GPIO23 as input with internal pull-up resistor to hold it HIGH
 # until it is pulled down to GND by the connected button:
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Register an edge detection event on FALLING edge. When this event
 # fires, the callback onButton() will be executed. Because of
-# bouncetime=20 all edges 20 ms after a first falling edge will be ignored:
+# bouncetime=500 all edges 500 ms after a first falling edge will be ignored:
 GPIO.add_event_detect(23, GPIO.FALLING, callback=onButton, bouncetime=500)
 
 # The script would exit now but we want to wait for the event to occure
